2058b1692160c1dca4bf63dd3d051c33057c5963
xie <283994917@qq.com>
2019-03-09 10:12:27 +0800

    init newsAPI and picture API

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,56 +1,64 @@
 (function () {
     const form = document.querySelector('#search-form');
     const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    // 使用Promise 监听 XHR，成功失败做出响应
+    let searchedForText,
+        articleText;
+    // 使用Promise 创建XHR
     function get(url) {
         return new Promise((resolve, reject) => {
-            var req = new XMLHttpRequest();
-            req.open('GET',url);
-            req.setRequestHeader('Authorization', 'Client-ID ');
-            req.onload = function () {
+            const req = new XMLHttpRequest();
+            req.open('GET', url);
+            req.onload = () => {
                 if (req.status === 200) {
                     resolve(req.responseText);
                 } else {
                     reject(Error(req.statusText));
                 }
             };
-            req.onerror = function () {
+            req.onerror = () => {
                 reject(Error('Network Error'));
             };
             req.send();
         });
     }
-
-    // 获取数据后，加入页面
-    function addImage(data) {
-        const firstImage = data.results[0];
-        console.log(data);
-        const htmlContent = `<figure>
-            <img src="${firstImage.urls.regular}" alt="Author of ${firstImage.user.name}">
-            <figcaption>Author by of ${firstImage.user.name}</figcaption>
-        </figure>`;
-        responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
+    // 将新闻放入列表
+    function addArticl(data) {
+        let htmlContent = '<ul>' + data.response.docs.map(b => `<li class="article">
+            <h2><a href="${b.web_url}">${b.headline.main}</a></h2>
+            <p>${b.snippet}</p>
+        </li>`).join('') + '</ul>';
+        responseContainer.insertAdjacentHTML('beforeend', htmlContent);
     }
-
-
     const responseContainer = document.querySelector('#response-container');
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         responseContainer.innerHTML = '';
-        searchedForText = `https://api.unsplash.com/search/photos?query=${searchField.value}`;
-        console.log(searchedForText);
+        searchedForText = `https://api.unsplash.com/search/photos?query=${searchField.value}&client_id=557354770f65429aaac1de0985053a29dad393d2d87dc9021e27fc339a24c1a1`;
+        articleText = `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchField.value}&api-key=s190ixVYfs7W8ddzklF90JndtEsKJiEt`;
+        // 相片API
         get(searchedForText)
-            .then((data) => {
-                return JSON.parse(data);
-            })
+            .then((data) => JSON.parse(data))
             .then((url) => {
-                addImage(url);
+                const image = url.results[0];
+                let html = `<figure>
+                    <img src="${image.urls.regular}" alt="the author is ${image.user.name}">
+                    <figcaption>Image author is ${image.user.name}</figcaption>
+                </figure>`;
+                responseContainer.insertAdjacentHTML('afterbegin', html);
             })
             .catch((error) => {
                 console.log(error);
-                responseContainer.innerHTML = 'No Search';
+                let html = `<h1>No search!</h1>`;
+                responseContainer.insertAdjacentHTML('afterbegin', html);
             });
-
+        // 纽约新闻API
+        get(articleText)
+            .then((data) => JSON.parse(data))
+            .then(addArticl)
+            .catch((error) => {
+                console.log(error);
+                let html = `<h1>No search!</h1>`;
+                responseContainer.insertAdjacentHTML('beforeend', html);
+            });
     });
 })();
